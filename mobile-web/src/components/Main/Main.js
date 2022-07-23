6507d50604394d082b1f242df2abb8463b5c9be0
TImmy <boss63081@gmail.com>
2022-07-23 17:56:05 +0800

    新增底欄

--- mobile-web/src/components/Main/Main.js
+++ mobile-web/src/components/Main/Main.js
@@ -1,13 +1,14 @@
 import React, {Component} from "react"
+import Bottombar from "../Tabbar/Bottombar";
 
 class Main extends Component {
     render() {
         return (
             <div>
                 <p> 首頁 </p>
+                <Bottombar/>
             </div>
         )
     }
 }
-
 export default Main
