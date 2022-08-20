0cfa73384271600e8f2e7b1e8e18ec88c0d3b1af
TImmy <boss63081@gmail.com>
2022-08-20 15:20:16 +0800

    添增bottombar跳轉功能

--- mobile-web/src/components/Tabbar/Bottombar.js
+++ mobile-web/src/components/Tabbar/Bottombar.js
@@ -1,4 +1,5 @@
-import React, {Component} from "react"
+import React from "react"
+import { useNavigate } from "react-router";
 import BottomNavigation from "reactjs-bottom-navigation"
 import "./Bottombar.css"
 import HomeOnClickPNG from '../../image/bottombar/home_onclick.png';
@@ -10,31 +11,34 @@ import AccountNoClickPNG from '../../image/bottombar/account_noclick.png';
 
 const bottomNavItems = [
     {
-        title:'首頁',
+        title:"首頁",
         icon: <img src={HomeNoClickPNG} alt="Home" />,
-        activeIcon: <img src={HomeOnClickPNG} alt="Home" />
+        activeIcon: <img src={HomeOnClickPNG} alt="Home" />,
+        path: "/"
     },
     {
-        title:'我的最愛',
+        title:"我的最愛",
         icon: <img src={FavoriteNoClickPNG} alt="Favorite" />,
-        activeIcon: <img src={FavoriteOnClickPNG} alt="Favorite" />
+        activeIcon: <img src={FavoriteOnClickPNG} alt="Favorite" />,
+        path: "/favorite"
     },
     {
-        title:'帳戶資訊',
+        title:"帳戶資訊",
         icon: <img src={AccountNoClickPNG} alt="Account" />,
-        activeIcon: <img src={AccountOnClickPNG} alt="Account" />
+        activeIcon: <img src={AccountOnClickPNG} alt="Account" />,
+        path: "/login"
     }
 ]
 
-class Bottombar extends Component {
-    render() {
-        return (
-            <BottomNavigation
-                items={bottomNavItems}
-                defaultSelected={0}
-                onItemClick={(item) => console.log(item)}
-            />
-        )
-    }
+const Bottombar = (props) => {
+    let navigate = useNavigate();
+    const currentIndex = props.index;
+    return (
+        <BottomNavigation
+            items={bottomNavItems}
+            defaultSelected={currentIndex}
+            onItemClick={(item) => navigate(item.path)}
+        />
+    );
 }
 export default Bottombar;
