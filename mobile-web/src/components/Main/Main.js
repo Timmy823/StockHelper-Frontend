e537b3d64bfdcf5d291a2ea69c32ac5f9365aa70
TImmy <boss63081@gmail.com>
2022-08-20 12:14:25 +0800

    SPA建構

--- mobile-web/src/components/Main/Main.js
+++ mobile-web/src/components/Main/Main.js
@@ -1,37 +1,22 @@
-import React, {Component} from "react"
+import React from "react"
+import { Routes, Route } from "react-router-dom";
+import Home from "../Home/Home";
+import Login from "../Form/Login";
 import Bottombar from "../Tabbar/Bottombar";
-import StockSearch from "../Search/StockSearch";
-import MainInstantStock from "../Card/MainInstantStock";
 
-class Main extends Component {
-    render() {
-        return (
-            <div>
-                <StockSearch/>
-                <div className='stock-home-page'>
-                    <div className='stock-index'>
-                        <h3> 指數 </h3>
-                        <MainInstantStock className='TAIEX-index'/>
-                        <MainInstantStock className='TWO-index'/>
-                    </div>
-                    <div className='hot-stock'>
-                        <h3> 熱門個股 </h3>
-                        <MainInstantStock className='stock_2330'/>
-                        <MainInstantStock className='stock_2498'/>
-                        <MainInstantStock className='stock_2331'/>
-                        <MainInstantStock className='stock_2492'/>
-                    </div>
-                    <div className='hot-etf'>
-                        <h3> 熱門ETF </h3>
-                        <MainInstantStock className='etf_2330'/>
-                        <MainInstantStock className='etf_2498'/>
-                        <MainInstantStock className='etf_2331'/>
-                        <MainInstantStock className='etf_2492'/>
-                    </div>
-                </div>
-                <Bottombar/>
-            </div>
-        )
-    }
+const Main = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<>
+                <Home />
+                <Bottombar />
+            </>} />
+            <Route path="/favorite" element={<Home/>} />
+            <Route path='/login' element={<>
+                <Login />
+                <Bottombar />
+            </>} />
+        </Routes>
+    );
 }
 export default Main
