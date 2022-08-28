889e3b006b5aa9b19fab8f1fc0124321657af49e
TImmy <boss63081@gmail.com>
2022-08-28 15:19:30 +0800

    註冊欄位驗證

--- mobile-web/src/pages/Account/Register.js
+++ mobile-web/src/pages/Account/Register.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from "yup";
 import {
   MDBContainer,
   MDBInput,
@@ -12,22 +14,80 @@ import './Account.css';
 
 const Register = () => {
     return (
-        <MDBContainer className="p-3 mt-1 mb-6 d-flex flex-column w-100">
+        <MDBContainer className="register-container p-3 mt-1 mb-6 d-flex flex-column w-100">
             <h3>註冊</h3>
-            <MDBInput wrapperClass='mt-4' label='電子郵件' id='form1' type='email'/>
-            <MDBInput wrapperClass='mt-4' label='密碼' id='form2' type='password'/>
-            <MDBInput wrapperClass='mt-4' label='密碼再次確認' id='form3' type='password'/>
-            <MDBInput wrapperClass='mt-4' label='名稱' id='form4' type='name'/>
-            <MDBInput wrapperClass='mt-4' label='手機號碼' id='form5' type='telphone'/>
+            <Formik
+                initialValues={{ 
+                    email: undefined, 
+                    password: undefined,
+                    password_confirm: undefined,
+                    name: undefined,
+                    telphone: undefined,
+                    agreecheckbox: false
+                }}
+                validationSchema={Yup.object({
+                    email: Yup.string()
+                        .email("*請輸入正確格式的信箱")
+                        .required("*電子信箱不能為空"),
+                    password: Yup.string()
+                        .matches(/^[A-Za-z0-9]{9,16}$/,"*密碼長度為9-16之間的大小寫英文字母或數字")
+                        .required("*密碼不能為空"),
+                    password_confirm: Yup.string()
+                        .when('password', (password, schema) => {
+                            return password ? schema.oneOf([password], '*密碼需相同').required() : schema
+                        }),
+                    name: Yup.string()
+                        .required("*名稱不能為空"),
+                    telphone: Yup.string()
+                        .matches(/^[0-9]{10,10}$/,"*電話為10碼數字"),
+                    agreecheckbox: Yup.boolean()
+                        .isTrue("需同意服務條款")
+                })}
+                onSubmit={(values, { resetForm }) => {
+                    console.log('submit');
+                    console.log(values);
+                    resetForm();
+                }}
+            >
+                {({errors, touched}) => (
+                    <Form>
+                        <Field as={MDBInput} wrapperClass='mt-1' label='電子郵件' id='form1' name='email'/>
+                        {errors.email && touched.email ? (
+                            <div className='error-message active'>{errors.email}</div>
+                        ) : <div className='error-message hidden'>正確格式</div>}
+ 
+                        <Field as={MDBInput} wrapperClass='mt-1' label='密碼' id='form2' name='password'/>
+                        {errors.password && touched.password ? (
+                            <div className='error-message active'>{errors.password}</div>
+                        ) : <div className='error-message hidden'>正確格式</div>}
 
-            <div className='d-flex justify-content-center mt-1 mb-1'>
-                <MDBCheckbox name='flexCheck' id='flexCheckDefault' label='I have read and agree to the terms' />
-            </div>
+                        <Field as={MDBInput} wrapperClass='mt-1' label='密碼再次確認' id='form3' name='password_confirm'/>
+                        {errors.password_confirm && touched.password_confirm ? (
+                            <div className='error-message active'>{errors.password_confirm}</div>
+                        ) : <div className='error-message hidden'>正確格式</div>}
 
-            <div className='d-flex w-100'>
-                <MDBBtn className="mb-2 mx-2 w-50">送出</MDBBtn>
-                <MDBBtn tag='a' href='/login' className="btn-light mb-2 mx-2 w-50">取消</MDBBtn>
-            </div> 
+                        <Field as={MDBInput} wrapperClass='mt-1' label='名稱' id='form4' name='name'/>
+                        {errors.name && touched.name ? (
+                            <div className='error-message active'>{errors.name}</div>
+                        ) : <div className='error-message hidden'>正確格式</div>}
+
+                        <Field as={MDBInput} wrapperClass='mt-1' label='手機號碼' id='form5' name='telphone'/>
+                        {errors.telphone && touched.telphone ? (
+                            <div className='error-message active'>{errors.telphone}</div>
+                        ) : <div className='error-message hidden'>正確格式</div>}
+
+                        <Field as={MDBCheckbox} className='mt-1 mb-1' label='我同意服務條款' id='flexCheckDefault' name='agreecheckbox'/>
+                        {errors.agreecheckbox && touched.agreecheckbox ? (
+                            <div className='error-message active'>{errors.agreecheckbox}</div>
+                        ) : <div className='error-message hidden'>正確格式</div>}
+
+                        <div className='d-flex w-100'>
+                            <MDBBtn className="mb-2 mx-2 w-50" type="submit">送出</MDBBtn>
+                            <MDBBtn tag='a' href='/login' className="btn-light mb-2 mx-2 w-50">取消</MDBBtn>
+                        </div>
+                    </Form> 
+                )}
+            </Formik>
             
             <div className="text-center">
                 <p>或 以下列方式登入:</p>
