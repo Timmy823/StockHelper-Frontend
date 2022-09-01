598d04b02d99a0a9dd8e12d3ce1998eef68714e5
TImmy <boss63081@gmail.com>
2022-09-01 23:15:13 +0800

    登入欄位驗證

--- mobile-web/src/pages/Account/Login.js
+++ mobile-web/src/pages/Account/Login.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from "yup";
 import {
   MDBContainer,
   MDBInput,
@@ -40,34 +42,57 @@ const Login = () => {
     return (
         <MDBContainer className="p-3 mt-1 d-flex flex-column w-100">
             <h3>登入</h3>
-            <MDBInput wrapperClass='mt-4' label='電子信箱' id='form1' name='account'/>
-            <MDBInput wrapperClass='mt-4' label='密碼' id='form2' name='password'/>
+            <Formik
+                initialValues={{ 
+                    account: undefined, 
+                    password: undefined
+                }}
+                validationSchema={Yup.object({
+                    account: Yup.string()
+                        .email("*請輸入正確格式的信箱")
+                        .required("*電子信箱不能為空"),
+                    password: Yup.string()
+                        .matches(/^[A-Za-z0-9]{9,16}$/,"*密碼長度為9-16之間的大小寫英文字母或數字")
+                        .matches(/^.*(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/, "*大小寫英文字母或數字必須皆有")
+                        .required("*密碼不能為空")
+                })}
+                onSubmit={(value, { resetForm }) => {
+                    Login({
+                        "member_account": value.account,
+                        "password": value.password
+                    }, (response) => {
+                        console.log(response);
+                        if(response.metadata.status === 'success') 
+                            navigate('/account');
+
+                        document.getElementsByClassName('login-message')[0].innerHTML = response.metadata.desc;
+                        document.getElementsByClassName('login-message')[0].classList.remove("hidden");
+                        document.getElementsByClassName('login-message')[0].classList.add("active");
+                    });
+                }}
+            >
+                {({errors, touched}) => (
+                    <Form>
+                        <Field as={MDBInput} wrapperClass='mt-1' label='電子郵件' id='form1' name='account'/>
+                        {errors.account && touched.account ? (
+                            <div className='error-message active'>{errors.account}</div>
+                        ) : <div className='error-message hidden'>正確格式</div>}
+
+                        <Field as={MDBInput} wrapperClass='mt-1' label='密碼' id='form2' name='password'/>
+                        {errors.password && touched.password ? (
+                            <div className='error-message active'>{errors.password}</div>
+                        ) : <div className='error-message hidden'>正確格式</div>}
+                    </Form>
+                )}
+            </Formik>
 
             <div className="d-flex justify-content-between mx-3 my-2">
                 <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='記住帳號密碼' />
                 <a href="!#">忘記密碼?</a>
             </div>
-
-            <div className='login-message hidden'> 註冊成功 </div>
-
-            <MDBBtn className="mb-4" onClick={() => {
-                let member_account = document.getElementsByName('account')[0].value;
-                let password = document.getElementsByName('password')[0].value;
-                Login({
-                    "member_account": member_account,
-                    "password": password
-                }, (response) => {
-                    console.log(response);
-                    if(response.metadata.status === 'success') 
-                        navigate('/account');
-
-                    document.getElementsByClassName('login-message')[0].innerHTML = response.metadata.desc;
-                    document.getElementsByClassName('login-message')[0].classList.remove("hidden");
-                    document.getElementsByClassName('login-message')[0].classList.add("active");
-                });
-
-            }}>登入</MDBBtn>
-
+            <div className='login-message hidden'> 登入成功 </div>
+            <MDBBtn className="mb-4">登入</MDBBtn>
+            
             <div className="text-center">
                 <p>尚未擁有帳號? <a href="/register">註冊</a></p>
                 <p>或 以下列方式登入:</p>
