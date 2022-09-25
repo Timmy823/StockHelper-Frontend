ec59c3b4df5399df71c3b931631cbbda6c192697
TImmy <boss63081@gmail.com>
2022-09-25 21:35:14 +0800

    暫時首頁按鈕

--- mobile-web/src/components/Search/StockSearch.js
+++ mobile-web/src/components/Search/StockSearch.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react"
+import React, { useEffect, useState } from "react"
 import { useNavigate } from 'react-router-dom';
 import CryptoJS from 'crypto-js';
 import "./StockSearch.css"
@@ -14,71 +14,71 @@ const StockSearch = () => {
     const [options, setOptions] = useState([]);
 
     const getCompanyList = async (type) => {
-      let request = await fetch('http://localhost:5277/twse/getAllCompanyList?type='+ type, {
-          method: 'GET',
-          headers: { 
-              'Content-Type': 'application/json',
-          }
-      });
+        let request = await fetch('http://localhost:5277/twse/getAllCompanyList?type=' + type, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
 
-      let response = await request.json();
-      if (request.status === 200) {
-          let result = response['data'].map((element)=>{
-              let value = {
-                  stock_name: element['Name'],
-                  stock_id: element['ID'],
-                  stock_type: type == '1'? '上市' : '上櫃',
-              };
+        let response = await request.json();
+        if (request.status === 200) {
+            let result = response['data'].map((element) => {
+                let value = {
+                    stock_name: element['Name'],
+                    stock_id: element['ID'],
+                    stock_type: type == '1' ? '上市' : '上櫃',
+                };
 
-              return {
-                  name: element['Name'],
-                  value: CryptoJS.AES.encrypt(
-                      JSON.stringify(value),
-                      CryptoJS.enc.Utf8.parse(secretKey),
-                      {iv: IV, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.ZeroPadding}
-                  ).ciphertext.toString(CryptoJS.enc.Hex)
-              };
-          });
+                return {
+                    name: element['Name'],
+                    value: CryptoJS.AES.encrypt(
+                        JSON.stringify(value),
+                        CryptoJS.enc.Utf8.parse(secretKey),
+                        { iv: IV, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.ZeroPadding }
+                    ).ciphertext.toString(CryptoJS.enc.Hex)
+                };
+            });
 
-          setOptions(prevState => prevState.concat(result));
-      }
+            setOptions(prevState => prevState.concat(result));
+        }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getCompanyList('1');
         getCompanyList('2');
-    },[])
-    
+    }, [])
+
     return (
         <div className='stock-search py-2'>
-            <div className=''>
-              首頁
-            </div>
+            <a className='' href='/'>
+                首頁
+            </a>
             <SelectSearch
-              options={options}
-              value={value}
-              onChange={(e)=>{
-                  navigate('/stock?id=' + e);
-              }}
-              search
-              emptyMessage="Not found"
-              placeholder="2330"
-              filterOptions={(options) => {
-                  const fuse = new Fuse(options, {
-                      keys: ["name","value"],
-                      threshold: 0.3
-                  });
-                
-                  return (value) => {
-                      if (!value.length) {
-                          return options;
-                      }
+                options={options}
+                value={value}
+                onChange={(e) => {
+                    navigate('/stock?id=' + e);
+                }}
+                search
+                emptyMessage="Not found"
+                placeholder="2330"
+                filterOptions={(options) => {
+                    const fuse = new Fuse(options, {
+                        keys: ["name", "value"],
+                        threshold: 0.3
+                    });
 
-                      return fuse.search(value).map((result) => result.item);
-                  };
-              }} />
+                    return (value) => {
+                        if (!value.length) {
+                            return options;
+                        }
+
+                        return fuse.search(value).map((result) => result.item);
+                    };
+                }} />
         </div>
-        
+
     );
 }
 export default StockSearch
