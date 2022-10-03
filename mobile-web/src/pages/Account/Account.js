f4d2eb779af86ecf11df0bc82fac6b0dd1ea7221
TImmy <boss63081@gmail.com>
2022-10-03 10:59:13 +0800

    帳戶資訊登入驗證

--- mobile-web/src/pages/Account/Account.js
+++ mobile-web/src/pages/Account/Account.js
@@ -19,7 +19,9 @@ const Account = () => {
 
     useEffect(()=>{
         if (getAuthToken(login_token) === null)
-            navigate('/login');
+            navigate('/login', {
+                state: '/account'
+            });
         setAccountInfomation(JSON.parse(getAuthToken(login_token)));
     }, [accountInfomation]);
 
