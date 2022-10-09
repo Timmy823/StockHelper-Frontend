d06a15fc4203c475993810201cc6113fddca37de
TImmy <boss63081@gmail.com>
2022-10-09 11:24:13 +0800

    新增編輯列表按鈕

--- mobile-web/src/pages/Favorite/Favorite.js
+++ mobile-web/src/pages/Favorite/Favorite.js
@@ -3,8 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { getAuthToken } from '../../utils/TokenUtils';
 import ListInstantStock from "../../components/Card/ListInstantStock";
 import "./Favorite.css";
-import { MDBNavbar, MDBNavbarBrand, MDBNavbarItem, MDBNavbarNav, MDBNavbarToggler } from 'mdb-react-ui-kit';
-import { propTypes } from 'react-bootstrap/esm/Image';
+import { MDBBtn, MDBContainer, MDBNavbar, MDBNavbarItem, MDBIcon } from 'mdb-react-ui-kit';
 
 const Favorite = () => {
     const navigate = useNavigate();
@@ -61,40 +60,41 @@ const Favorite = () => {
     }, []);
 
     return (
-        <>
-            <div className='favorite-page'>
-                <MDBNavbar className='option-button-list'>
-                    {FavoriteList.map((list, index) => {
-                        initialClass = (index == 0) ? 'option-button selected' : 'option-button';
-                        return <MDBNavbarItem className={initialClass}
-                            key={index}
-                            id={index}
-                            onClick={(e) => {
-                                setButtonIndex(e.target.id);
-                                FavoriteList.map((list, index) => {
-                                    if (document.getElementsByClassName('option-button')[index].classList.contains("selected"))
-                                        document.getElementsByClassName('option-button')[index].classList.remove("selected");
-                                })
-                                document.getElementsByClassName('option-button')[e.target.id].classList.add("selected");
-                            }}
-                        >{list.list_name}</MDBNavbarItem>;
-                    })}
-                </MDBNavbar>
-                <div className='stock-list'>
-                    {FavoriteList[ButtonIndex]['stock_list'].map((stock, index) => {
-                        if (stock.stock_id == '0000')
-                            return;
-                        return <><ListInstantStock className={'stockid_' + stock.stock_id} key={index} input_data={{
-                            'stock_name': stock.stock_name,
-                            'stock_id': stock.stock_id,
-                            'stock_type': ' ',
-                            'stock_value': '203.00',
-                            'stock_value_offset': '-1.23%'
-                        }} /><hr /></>;
-                    })}
-                </div>
+        <MDBContainer className='favorite-page'>
+            <MDBNavbar className='option-button-list'>
+                {FavoriteList.map((list, index) => {
+                    initialClass = (index == 0) ? 'option-button selected' : 'option-button';
+                    return <MDBNavbarItem className={initialClass}
+                        key={index}
+                        id={index}
+                        onClick={(e) => {
+                            setButtonIndex(e.target.id);
+                            FavoriteList.map((list, index) => {
+                                if (document.getElementsByClassName('option-button')[index].classList.contains("selected"))
+                                    document.getElementsByClassName('option-button')[index].classList.remove("selected");
+                            })
+                            document.getElementsByClassName('option-button')[e.target.id].classList.add("selected");
+                        }}
+                    >{list.list_name}</MDBNavbarItem>;
+                })}
+            </MDBNavbar>
+            <div className='stock-list'>
+                {FavoriteList[ButtonIndex]['stock_list'].map((stock, index) => {
+                    if (stock.stock_id == '0000')
+                        return;
+                    return <><ListInstantStock className={'stockid_' + stock.stock_id} key={index} input_data={{
+                        'stock_name': stock.stock_name,
+                        'stock_id': stock.stock_id,
+                        'stock_type': ' ',
+                        'stock_value': '203.00',
+                        'stock_value_offset': '-1.23%'
+                    }} /><hr /></>;
+                })}
             </div>
-        </>
+            <MDBBtn floating color='warning' className='mx-1' title='編輯列表' style={{ color: '#fff' }}>
+                <MDBIcon far icon="edit" size='lg'/> 編輯列表
+            </MDBBtn>
+        </MDBContainer>
     );
 }
 export default Favorite;
