a2f4913893783dd23080d7b952ae822f86d7a6d2
TImmy <boss63081@gmail.com>
2022-10-15 19:37:57 +0800

    調整樣式

--- mobile-web/src/pages/Home/Home.js
+++ mobile-web/src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import { MDBContainer } from "mdb-react-ui-kit";
 import MainInstantStock from "../../components/Card/MainInstantStock";
 import "./Home.css"
 
@@ -117,170 +118,158 @@ const Home = () => {
         getHotETFTrend('00892');
     }, []);
 
-    useEffect(() => {
-        console.log(indexTrend);
-    },[indexTrend]);
-
-    useEffect(() => {
-    },[stockTrend]);
-
-    useEffect(() => {
-    },[etfTrend]);
-
     return (
-        <div>
-            <div className='stock-home-page'>
-                <div className='stock-index'>
-                    <h3> 指數 </h3>
-                    <MainInstantStock 
-                        className='TAIEX-index' 
-                        stock={{
-                            'stock_name': '加權指數',
-                            'stock_id': ' ',
-                            'stock_type': ' ',
-                            'stock_value': getLastClosingPrice(indexTrend['2330']),
-                            'stock_value_offset': getStcokOffset(indexTrend['2330']),
-                        }}
-                        trend_data={[{
-                            'name': 'index',
-                            'data': indexTrend['2330']
-                        }]}
-                    />
-                    <MainInstantStock 
-                        className='TWO-index' 
-                        stock={{
-                            'stock_name': '櫃買指數',
-                            'stock_id': ' ',
-                            'stock_type': ' ',
-                            'stock_value': getLastClosingPrice(indexTrend['2364']),
-                            'stock_value_offset': getStcokOffset(indexTrend['2364']),
-                        }}
-                        trend_data={[{
-                            'name': 'index',
-                            'data': indexTrend['2364']
-                        }]}
-                    />
-                </div>
-                <div className='hot-stock'>
-                    <h3> 熱門個股 </h3>
-                    <MainInstantStock 
-                        className='stock_2330' 
-                        stock={{
-                            'stock_name': '台積電',
-                            'stock_id': '2330',
-                            'stock_type': '上市',
-                            'stock_value': getLastClosingPrice(stockTrend['2330']),
-                            'stock_value_offset': getStcokOffset(stockTrend['2330']),
-                        }}
-                        trend_data={[{
-                            'name': 'index',
-                            'data': stockTrend['2330']
-                        }]}
-                    />
-                    <MainInstantStock 
-                        className='stock_5483' 
-                        stock={{
-                            'stock_name': '中美晶',
-                            'stock_id': '5483',
-                            'stock_type': '上櫃',
-                            'stock_value': getLastClosingPrice(stockTrend['5483']),
-                            'stock_value_offset': getStcokOffset(stockTrend['5483']),
-                        }}
-                        trend_data={[{
-                            'name': 'index',
-                            'data': stockTrend['5483']
-                        }]}
-                    />
-                    <MainInstantStock 
-                        className='stock_1513' 
-                        stock={{
-                            'stock_name': '中興電',
-                            'stock_id': '1513',
-                            'stock_type': '上市',
-                            'stock_value': getLastClosingPrice(stockTrend['1513']),
-                            'stock_value_offset': getStcokOffset(stockTrend['1513']),
-                        }}
-                        trend_data={[{
-                            'name': 'index',
-                            'data': stockTrend['1513']
-                        }]}
-                    />
-                    <MainInstantStock 
-                        className='stock_2454' 
-                        stock={{
-                            'stock_name': '聯發科',
-                            'stock_id': '2454',
-                            'stock_type': '上市',
-                            'stock_value': getLastClosingPrice(stockTrend['2454']),
-                            'stock_value_offset': getStcokOffset(stockTrend['2454']),
-                        }}
-                        trend_data={[{
-                            'name': 'index',
-                            'data': stockTrend['2454']
-                        }]}
-                    />
-                </div>
-                <div className='hot-etf'>
-                    <h3> 熱門ETF </h3>
-                    <MainInstantStock 
-                        className='etf_0050' 
-                        stock={{
-                            'stock_name': '元大台灣50',
-                            'stock_id': '0050',
-                            'stock_type': '上市',
-                            'stock_value': getLastClosingPrice(etfTrend['0050']),
-                            'stock_value_offset': getStcokOffset(etfTrend['0050']),
-                        }}
-                        trend_data={[{
-                            'name': 'index',
-                            'data': etfTrend['0050']
-                        }]}
-                    />
-                    <MainInstantStock 
-                        className='etf_0056' 
-                        stock={{
-                            'stock_name': '元大高股息',
-                            'stock_id': '0056',
-                            'stock_type': '上市',
-                            'stock_value': getLastClosingPrice(etfTrend['0056']),
-                            'stock_value_offset': getStcokOffset(etfTrend['0056']),
-                        }}
-                        trend_data={[{
-                            'name': 'index',
-                            'data': etfTrend['0056']
-                        }]}
-                    />
-                    <MainInstantStock 
-                        className='etf_00878' 
-                        stock={{
-                            'stock_name': '國泰永續高股息',
-                            'stock_id': '00878',
-                            'stock_type': '上市',
-                            'stock_value': getLastClosingPrice(etfTrend['00878']),
-                            'stock_value_offset': getStcokOffset(etfTrend['00878']),
-                        }}
-                        trend_data={[{
-                            'name': 'index',
-                            'data': etfTrend['00878']
-                        }]}
-                    />
-                    <MainInstantStock 
-                        className='etf_00892' 
-                        stock={{
-                            'stock_name': '富邦台灣半導體',
-                            'stock_id': '00892',
-                            'stock_type': '上市',
-                            'stock_value': getLastClosingPrice(etfTrend['00892']),
-                            'stock_value_offset': getStcokOffset(etfTrend['00892']),
-                        }}
-                        trend_data={[{
-                            'name': 'index',
-                            'data': etfTrend['00892']
-                        }]}
-                    />
-                </div>
+        <MDBContainer className='stock-home-page'>
+            <div className='stock-index'>
+                <h3> 指數 </h3>
+                <MainInstantStock 
+                    className='TAIEX-index' 
+                    stock={{
+                        'stock_name': '加權指數',
+                        'stock_id': ' ',
+                        'stock_type': ' ',
+                        'stock_value': getLastClosingPrice(indexTrend['2330']),
+                        'stock_value_offset': getStcokOffset(indexTrend['2330']),
+                    }}
+                    trend_data={[{
+                        'name': 'index',
+                        'data': indexTrend['2330']
+                    }]}
+                />
+                <MainInstantStock 
+                    className='TWO-index' 
+                    stock={{
+                        'stock_name': '櫃買指數',
+                        'stock_id': ' ',
+                        'stock_type': ' ',
+                        'stock_value': getLastClosingPrice(indexTrend['2364']),
+                        'stock_value_offset': getStcokOffset(indexTrend['2364']),
+                    }}
+                    trend_data={[{
+                        'name': 'index',
+                        'data': indexTrend['2364']
+                    }]}
+                />
             </div>
-        </div>
+            <div className='hot-stock'>
+                <h3> 熱門個股 </h3>
+                <MainInstantStock 
+                    className='stock_2330' 
+                    stock={{
+                        'stock_name': '台積電',
+                        'stock_id': '2330',
+                        'stock_type': '上市',
+                        'stock_value': getLastClosingPrice(stockTrend['2330']),
+                        'stock_value_offset': getStcokOffset(stockTrend['2330']),
+                    }}
+                    trend_data={[{
+                        'name': 'index',
+                        'data': stockTrend['2330']
+                    }]}
+                />
+                <MainInstantStock 
+                    className='stock_5483' 
+                    stock={{
+                        'stock_name': '中美晶',
+                        'stock_id': '5483',
+                        'stock_type': '上櫃',
+                        'stock_value': getLastClosingPrice(stockTrend['5483']),
+                        'stock_value_offset': getStcokOffset(stockTrend['5483']),
+                    }}
+                    trend_data={[{
+                        'name': 'index',
+                        'data': stockTrend['5483']
+                    }]}
+                />
+                <MainInstantStock 
+                    className='stock_1513' 
+                    stock={{
+                        'stock_name': '中興電',
+                        'stock_id': '1513',
+                        'stock_type': '上市',
+                        'stock_value': getLastClosingPrice(stockTrend['1513']),
+                        'stock_value_offset': getStcokOffset(stockTrend['1513']),
+                    }}
+                    trend_data={[{
+                        'name': 'index',
+                        'data': stockTrend['1513']
+                    }]}
+                />
+                <MainInstantStock 
+                    className='stock_2454' 
+                    stock={{
+                        'stock_name': '聯發科',
+                        'stock_id': '2454',
+                        'stock_type': '上市',
+                        'stock_value': getLastClosingPrice(stockTrend['2454']),
+                        'stock_value_offset': getStcokOffset(stockTrend['2454']),
+                    }}
+                    trend_data={[{
+                        'name': 'index',
+                        'data': stockTrend['2454']
+                    }]}
+                />
+            </div>
+            <div className='hot-etf'>
+                <h3> 熱門ETF </h3>
+                <MainInstantStock 
+                    className='etf_0050' 
+                    stock={{
+                        'stock_name': '元大台灣50',
+                        'stock_id': '0050',
+                        'stock_type': '上市',
+                        'stock_value': getLastClosingPrice(etfTrend['0050']),
+                        'stock_value_offset': getStcokOffset(etfTrend['0050']),
+                    }}
+                    trend_data={[{
+                        'name': 'index',
+                        'data': etfTrend['0050']
+                    }]}
+                />
+                <MainInstantStock 
+                    className='etf_0056' 
+                    stock={{
+                        'stock_name': '元大高股息',
+                        'stock_id': '0056',
+                        'stock_type': '上市',
+                        'stock_value': getLastClosingPrice(etfTrend['0056']),
+                        'stock_value_offset': getStcokOffset(etfTrend['0056']),
+                    }}
+                    trend_data={[{
+                        'name': 'index',
+                        'data': etfTrend['0056']
+                    }]}
+                />
+                <MainInstantStock 
+                    className='etf_00878' 
+                    stock={{
+                        'stock_name': '國泰永續高股息',
+                        'stock_id': '00878',
+                        'stock_type': '上市',
+                        'stock_value': getLastClosingPrice(etfTrend['00878']),
+                        'stock_value_offset': getStcokOffset(etfTrend['00878']),
+                    }}
+                    trend_data={[{
+                        'name': 'index',
+                        'data': etfTrend['00878']
+                    }]}
+                />
+                <MainInstantStock 
+                    className='etf_00892' 
+                    stock={{
+                        'stock_name': '富邦台灣半導體',
+                        'stock_id': '00892',
+                        'stock_type': '上市',
+                        'stock_value': getLastClosingPrice(etfTrend['00892']),
+                        'stock_value_offset': getStcokOffset(etfTrend['00892']),
+                    }}
+                    trend_data={[{
+                        'name': 'index',
+                        'data': etfTrend['00892']
+                    }]}
+                />
+            </div>
+        </MDBContainer>
     );
 }
 export default Home;
